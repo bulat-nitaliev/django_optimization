ba8167958a7c1f6d709b679986f7852a7a782da3
bulat <nibulat924@gmail.com>
2024-06-28 16:52:09 +0400

    docker drf annotate aggregate

---
+++ service/generals/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+from clients.models import Client
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+    plan_types = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+
+    def __str__(self) -> str:
+        return self.plan_types
+
+class Subscription(models.Model):
+    client = models.ForeignKey(to=Client, on_delete=models.PROTECT, related_name='subscriptions')
+    service = models.ForeignKey(to=Service, on_delete=models.PROTECT, related_name='subscriptions')
+    plan = models.ForeignKey(to=Plan, on_delete=models.PROTECT, related_name='subscriptions')
